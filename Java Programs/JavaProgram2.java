70e86f23155b30c0ea4b9e26fae6ff9aed47e9f1
Jovi Ma <s3947223@student.rmit.edu.au>
2022-05-09 12:48:27 +1000

    Enter

---
+++ Java Programs/JavaProgram2.java
@@ -0,0 +1,11 @@
+package Java Programs;
+import java.util.Scanner;
+
+public class JavaProgram2 {
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+        int i;
+        
+
+    
+}
