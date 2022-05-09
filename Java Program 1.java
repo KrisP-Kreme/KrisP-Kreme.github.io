1cc30d905299af69fa3239f4e2de197a3c3a8b02
Jovi Ma <s3947223@student.rmit.edu.au>
2022-05-09 11:54:07 +1000

    added java page

---
+++ Java Program 1.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Method {
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+        int amountChips;
+        int amountNuggets;
+        int amountHotdogs;
+        double chipsCost = 2.50;
+        double nuggetsCost = 1.10;
+        double hotdogsCost; = 4.00;
+        double totalCost;
+
+        System.out.println("Enter the amount of Chips you would like");
+        amountChips = scnr.nextInt();
+
+        System.out.println("Enter the amount of Nuggets you would like")
+        amountNuggets = scnr.nextInt();
+
+        System.out.println("Enter the amount of Hotdogs you would like")
+        amountHotdogs = scnr.nextInt();
+
+        double chips = chipsCost * amountChips;
+        double nuggets = nuggetsCost * amountNuggets;
+        double hotdogs = hotdogsCost * amountHotdogs;
+        
+        double totalCost = chips + nuggets + hotdogs;
+
+        System.out.println("The total cost of your order is " + totalCost);
+
+        
+    }
+}
